27f58c2d4ddec8ccb72778d19a021abd1611903a
vincmatheo <vincmatheo@gmail.com>
2024-10-09 13:08:05 +0000

    ok again?

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header: React.FC = () => {
+  return (
+    <header className=''>
+      <h1>My Awesome Website</h1>
+    </header>
+  );
+};
+
+export default Header;
