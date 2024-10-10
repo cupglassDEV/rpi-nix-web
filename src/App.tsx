72409e56994f730dc20dc4a7f0f0261e4f12e02d
vincmatheo <vincmatheo@gmail.com>
2024-10-10 06:23:35 +0000

    siuuu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Head from './components/Head'
 import Md from '@uiw/react-markdown-preview';
 import { Link } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
       <><Head></Head><div>
